accfb1724aa99f814cce0d437e5f340a87f7fa03
yamamoto-sanchi <103275096+yamamotoxqx@users.noreply.github.com>
2024-09-05 13:28:16 +0000

    init commit

---
+++ popup.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggle = document.getElementById('toggle');
+  
+    // 保存されている設定を読み込む
+    chrome.storage.sync.get('isEnabled', function(data) {
+      toggle.checked = data.isEnabled || false; // デフォルトはオフ
+    });
+  
+    toggle.addEventListener('change', function() {
+      // 設定を保存する
+      chrome.storage.sync.set({ isEnabled: this.checked });
+    });
+  });
