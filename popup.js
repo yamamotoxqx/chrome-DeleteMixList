d574948bf90fd628bd03b42c17b85f53ad5db45b
yamamoto-sanchi <103275096+yamamotoxqx@users.noreply.github.com>
2024-09-05 14:37:09 +0000

    デフォルトでオフに変更しました

--- popup.js
+++ popup.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const toggle = document.getElementById('toggle');
-  
-    // 保存されている設定を読み込む
-    chrome.storage.sync.get('isEnabled', function(data) {
-      toggle.checked = data.isEnabled || true; // デフォルトはおん
-    });
-  
-    toggle.addEventListener('change', function() {
-      // 保存
-      chrome.storage.sync.set({ isEnabled: this.checked });
-    });
+  const toggle = document.getElementById('toggle');
+
+  // 保存されている設定を読み込む
+  chrome.storage.sync.get('isEnabled', function(data) {
+    toggle.checked = data.isEnabled || false; // デフォルトはおん
   });
+
+  toggle.addEventListener('change', function() {
+    // 設定を保存
+    chrome.storage.sync.set({ isEnabled: this.checked });
+  });
+});
