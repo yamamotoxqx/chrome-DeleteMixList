d4bfedd043f6fc5594932bf1f7650d73d02e0f39
yamamoto-sanchi <103275096+yamamotoxqx@users.noreply.github.com>
2024-09-07 07:55:08 +0000

    secrity patch

--- background.js
+++ background.js
@@ -1,9 +1,55 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-  // 設定が有効になっているか確認する
-  chrome.storage.sync.get('isEnabled', function(data) {
-    if (data.isEnabled && details.url.startsWith('https://www.youtube.com/') && details.url.includes('&list=')) {
-      const newUrl = details.url.split('&list=')[0];
-      chrome.tabs.update(details.tabId, { url: newUrl });
-    }
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeNetRequest.updateDynamicRules({
+    addRules: [
+      {
+        id: 1,
+        priority: 1,
+        action: {
+          type: "modifyHeaders",
+          requestHeaders: [
+            {
+              header: "Referer",
+              operation: "remove",
+            },
+          ],
+        },
+        condition: {
+          urlFilter: "*://www.youtube.com/*&list=*",
+          resourceTypes: ["main_frame"],
+        },
+      },
+    ],
+    removeRuleIds: [1],
   });
+});
+
+chrome.storage.onChanged.addListener((changes) => {
+  if (changes.isEnabled) {
+    chrome.declarativeNetRequest.updateDynamicRules({
+      addRules: changes.isEnabled.newValue
+        ? [
+            {
+              id: 1,
+              priority: 1,
+              action: {
+                type: "redirect",
+                redirect: {
+                  regexSubstitution: "^(.*)&list=.*$",
+                  transform: {
+                    queryTransform: {
+                      removeParams: ["list"],
+                    },
+                  },
+                },
+              },
+              condition: {
+                urlFilter: "*://www.youtube.com/*&list=*",
+                resourceTypes: ["main_frame"],
+              },
+            },
+          ]
+        : [],
+      removeRuleIds: changes.isEnabled.newValue ? [] : [1],
+    });
+  }
 });
